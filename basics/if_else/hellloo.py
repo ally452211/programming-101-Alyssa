e826b470b9624869631ab1eccb0532c4541f3417
Billan Liao <billanliao@Billans-iMac.local>
2026-04-30 16:07:50 -0400

    this is my homework please be gentle fragile contants

---
+++ basics/if_else/hellloo.py
@@ -0,0 +1,34 @@
+x = input()
+if x == ("hi") or x== ("hello"):
+    print("hello human lets play a game ")
+else:
+     print("say hi or hello")
+
+x = input()
+if x == ("okay") or x== ("ok"):
+    print("You are faced with a crossroad all you have to choose is left or right")
+else:   
+     print("please say ok or okay")
+x = input()
+if x == ("left") :
+    print("you find yourself in a dark room you see a door do you open it? please answer yes or no")
+elif x == ("right") :
+    print("you find yourself at home with a dog licking your face you just woke up the end")
+else:
+    print("try again")
+x = input()
+if x==("yes"):
+    print("you see a canvas and pencils you can either draw a star,triangle or a heart")
+elif x==("no") :
+    print("well the door closed behind you and you die of thirst the end you suck")
+else:
+     print("try again")
+x = input()
+if x == "triangle":
+        print("the triangle opens into a portal and you escape")
+elif x == "heart":
+        print("the heart begins beating and gives you a second chance at life")
+if x==("star"):
+    print("the star glows and you are transported into space and you die")
+else:
+     print ("try again")

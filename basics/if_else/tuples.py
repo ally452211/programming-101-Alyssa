59d75b9488c07e3880f2a613cc3776a202261b19
alyssa <ally452211@gmail.com>
2026-05-16 17:11:20 -0400

    tuples v1

---
+++ basics/if_else/tuples.py
@@ -0,0 +1,7 @@
+print("ID")
+person = ("Alyssa", 12, "december 6", "red")
+
+print(f"Name: {person[0]}")
+print(f"Age: {person[1]}")
+print(f"Birthday: {person[2]}")
+print(f"Favourite Colour: {person[3]}")

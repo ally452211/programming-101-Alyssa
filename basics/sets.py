9b20f2c04919e3a37c2e9b4dfbe2cf07e9606ad8
alyssa <ally452211@gmail.com>
2026-05-18 11:11:24 -0400

    sets v1

---
+++ basics/sets.py
@@ -0,0 +1,15 @@
+invited = {"Alyssa", "sir henry the third", "daniel", "yoyo","terrance","Young sheldon","Mr beast"}
+showed_up = {"Alyssa", "daniel", "yoyo","terrance","bob","donald trump","karen",}
+
+no_show = invited - showed_up
+crashed = showed_up - invited
+
+print(f"lets see who showed up: {' '.join(showed_up)}")
+
+for person in no_show:
+    print(f"{person} didn't show up")
+
+for person in crashed:
+    print(f"{person} crashed the party ")
+
+print("this is a mess")

2302d3e6a6c2b85bf284d74802c896515ed03ef0
Joseph Liao <100002111+JosephLiao542211@users.noreply.github.com>
2026-06-18 18:32:03 -0400

    first pygame stuff

---
+++ basics/game.py
@@ -0,0 +1,29 @@
+import pygame 
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 640))
+
+text_font = pygame.font.SysFont("Arial", 30)
+
+def draw_text(text, font, text_col, x, y):
+    
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+running = True
+while running:
+
+    screen.fill((255, 255, 255)) 
+    
+    
+    draw_text("Hi Game", text_font, (0, 0, 0), 220, 150)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    
+    pygame.display.flip()
+
+pygame.quit()
